8dfef77c7ca536f117fc39245e6242382dd1c055
liuwenzheng <329541594@qq.com>
2026-05-08 15:24:25 +0800

    support multiple

--- mokoBleLib/src/main/java/com/moko/ble/lib/event/ConnectStatusEvent.java
+++ mokoBleLib/src/main/java/com/moko/ble/lib/event/ConnectStatusEvent.java
@@ -1,7 +1,18 @@
 package com.moko.ble.lib.event;
 
+import android.bluetooth.BluetoothDevice;
+
 public class ConnectStatusEvent {
     private String action;
+    private BluetoothDevice bluetoothDevice;
+
+    public BluetoothDevice getBluetoothDevice(){
+        return bluetoothDevice;
+    }
+
+    public void setBluetoothDevice(BluetoothDevice bluetoothDevice){
+        this.bluetoothDevice = bluetoothDevice;
+    }
 
     public String getAction() {
         return action;
